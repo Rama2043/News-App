91408fffff93665a86d731b893406105ab7471e1
Sri Ramakrishnan <123226563+Rama2043@users.noreply.github.com>
2025-10-31 18:08:50 +0530

    Remove key from api_and_headers.dart
    Removed the hardcoded API key for security reasons.

--- lib/controller/services/api_and_headers.dart
+++ lib/controller/services/api_and_headers.dart
@@ -1,4 +1,4 @@
-String apiKey = "2bec9d0341f848cab6d0080a89770a08";
+String apiKey = "";
 String country = "us";
 String newsAPI =
     "https://newsapi.org/v2/top-headlines?country=$country&apiKey=$apiKey";
